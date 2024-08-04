53afb4c59a1dc19f61dadf44295033a3fbc28dce
dayanaedwin <dayanaedwin2016@gmail.com>
2024-08-04 11:42:34 +0530

    adjust the color props of payment details card

--- frontend/src/components/PaymentDetails.tsx
+++ frontend/src/components/PaymentDetails.tsx
@@ -7,14 +7,14 @@ interface PaymentDetailsProps {
 
 export const PaymentDetails: React.FC<PaymentDetailsProps> = ({products, totalPrice}) => {
     return(
-        <div className="bg-green-100 border border-green-100 rounded-md p-4 text-xs space-y-2">
+        <div className="bg-red-100 border border-red-100 rounded-md p-4 text-xs space-y-2">
             {products.map((product: any) => (
                 <div key={product.id} className="flex justify-between items-center">
                     <p className="text-xs font-normal">{product.productDetails.name}</p>
                     <p className="text-xs">₹ {product.price}</p>
                 </div>
             ))}
-            <hr className="text-white"/>
+            <hr className="border-white border"/>
             <div className="flex justify-between items-center">
                     <p className="text-sm">Total</p>
                     <p>₹ {totalPrice}</p>
