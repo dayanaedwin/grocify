dd9dc466718f60f6f8912ae8b334882130b6637c
dayanaedwin <dayanaedwin2016@gmail.com>
2024-08-15 20:38:47 +0530

    move register and login apis to auth controller

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const route = express.Router();
 
+const authRoute = require('./authRoute');
 const userRoute = require('./userRoute');
 const productRoute = require('./productRoute');
 const userOrderRoute = require('./userOrderRoute');
 const cartRoute = require('./cartRoute');
 
+route.use('/auth', authRoute);
 route.use('/user', userRoute);
 route.use('/product', productRoute);
 route.use('/my-order', userOrderRoute);
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -3,12 +3,9 @@ const router = express.Router();
 const { auth } = require('../middleware/authMiddleware');
 const controller = require('../controllers/userController');
 
-router.post('/register', controller.registerUser);
-router.post('/login', controller.loginUser);
-
-router.put('/update-password', auth, controller.updateUserPassword);
-
 router.get('/me', auth, controller.getCurrentUser);
 router.put('/me', auth, controller.updateUser);
+
+router.put('/update-password', auth, controller.updateUserPassword);
 
 module.exports = router;
